ad969f91c395d785bbba412cbdc4121bb58f5d07
Shital Ghatole <shital.ghatole@kraftpixel.com>
2026-03-23 18:45:07 +0530

    chore: add links and minor css adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
 
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>My Next.js App</h1>
+      <h1 className={styles.title}>Learning Paths</h1>
 
-      <p className={styles.description}>Following is the sitemap of all the available clickable routes</p>
+      <p className={styles.description}>Following is the sitemap of all the available clickable routes. Visit this <a href="https://github.com/shital-kp/smart-goals-first-qt-26" target="_blank" rel="noopener noreferrer">repo</a> for the source code.</p>
       <RoutesSitemaps />
     </div>
   );
